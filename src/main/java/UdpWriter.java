abb33aacfe705f9c9bb3e475f91f1fb9931fb9a6
joern <joernspam@gmx.de>
2021-10-09 16:12:56 +0200

    udp src/dest ports split, java version set to 11

--- src/main/java/UdpWriter.java
+++ src/main/java/UdpWriter.java
@@ -3,18 +3,19 @@ import java.net.*;
 import java.util.function.Consumer;
 
 public class UdpWriter implements Consumer<byte[]> {
+    private static final int LOCAL_PORT = 9999;
     private final InetAddress targetHost;
-    private final int port;
+    private final int targetPort;
 
-    UdpWriter(String host, int port) throws UnknownHostException {
+    UdpWriter(String host, int targetPort) throws UnknownHostException {
         targetHost = Inet4Address.getByName(host);
-        this.port = port;
+        this.targetPort = targetPort;
     }
 
     @Override
     public void accept(byte[] bytes) {
-        DatagramPacket packet = new DatagramPacket(bytes, bytes.length);
-        try (DatagramSocket socket = new DatagramSocket(port, targetHost)) {
+        DatagramPacket packet = new DatagramPacket(bytes, bytes.length, targetHost, targetPort);
+        try (DatagramSocket socket = new DatagramSocket(LOCAL_PORT)) {
             socket.send(packet);
         } catch (IOException e) {
             e.printStackTrace();
